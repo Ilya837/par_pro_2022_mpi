fde48c00d51738e451f07025d0af0421b3245123
Mikerin Ilya <mikerin-2002@mail.ru>
2022-12-23 23:15:51 +0300

    fix

--- modules/task_1/Mikerin_I_max_diff/max_diff.cpp
+++ modules/task_1/Mikerin_I_max_diff/max_diff.cpp
@@ -34,41 +34,53 @@ int getParallelOperations(int* global_vec, int count_size_vector) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     const int delta = count_size_vector / size;
     const int remainder = count_size_vector % size;
+    int* sendcount = new int[size];
+    int* displs = new int[size];
 
-    if (rank == 0) {
+    sendcount[0] = delta+1 + static_cast<int>(remainder > 0);
+    displs[0] = 0;
+
         if (remainder > 0) {
             for (int proc =1; proc < remainder -1; proc++) {
-                MPI_Send(global_vec + proc * delta + proc, delta+2, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                sendcount[proc] = delta + 2;
+                displs[proc] = proc * delta + proc;
             }
             for (int proc = remainder - 1; proc < size-1; proc++) {
                 if (proc > 0) {
-                    MPI_Send(global_vec + proc * delta + remainder, delta+1, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                    sendcount[proc] = delta + 1;
+                    displs[proc] = proc * delta + remainder;
                 }
             }
-            MPI_Send(global_vec + (size-1) * delta + remainder, delta, MPI_INT, (size-1), 0, MPI_COMM_WORLD);
+            sendcount[size-1] = delta;
+            displs[size-1] = (size-1) * delta + remainder;
         } else {
             for (int proc = 1; proc < size-1; proc++) {
-                MPI_Send(global_vec + proc * delta, delta+1, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                sendcount[proc] = delta + 1;
+                displs[proc] = proc * delta;
             }
-            MPI_Send(global_vec + (size-1) * delta, delta, MPI_INT, (size-1), 0, MPI_COMM_WORLD);
+            sendcount[size-1] = delta;
+            displs[size-1] = (size-1) * delta + remainder;
         }
-    }
+    //}
 
     int global_max_diff = 0;
 
     if (rank == size-1) {
         int* local_vec = new int[delta];
         MPI_Status status;
-        MPI_Recv(local_vec, delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Scatterv(global_vec , sendcount , displs , MPI_INT ,
+                    local_vec , sendcount[rank] , MPI_INT , 0 , MPI_COMM_WORLD);
         int local_max_diff = getSequentialOperations(local_vec, delta);
 
         MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
     } else {
         if (rank == 0) {
             int* local_vec = new int[delta+1 + static_cast<int>(remainder > 0)];
-            for (int i = 0; i < delta+ 1 + static_cast<int>(remainder > 0); i++) {
-                local_vec[i] = global_vec[i];
-            }
+            // for (int i = 0; i < delta+ 1 + static_cast<int>(remainder > 0); i++) {
+            //     local_vec[i] = global_vec[i];
+            // }
+            MPI_Scatterv(global_vec , sendcount , displs , MPI_INT ,
+                    local_vec , sendcount[rank] , MPI_INT , 0 , MPI_COMM_WORLD);
             int local_max_diff = getSequentialOperations(local_vec, delta+1 + static_cast<int>(remainder > 0));
             MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 
@@ -77,21 +89,26 @@ int getParallelOperations(int* global_vec, int count_size_vector) {
                 if (rank < remainder - 1) {
                     int* local_vec = new int[delta+2];
                     MPI_Status status;
-                    MPI_Recv(local_vec, delta + 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+                    MPI_Scatterv(global_vec , sendcount , displs , MPI_INT ,
+                    local_vec , sendcount[rank] , MPI_INT , 0 , MPI_COMM_WORLD);
                     int local_max_diff = getSequentialOperations(local_vec, delta+2);
                     MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 
                 } else {
                     int* local_vec = new int[delta+1];
                     MPI_Status status;
-                    MPI_Recv(local_vec, delta + 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+                    MPI_Scatterv(global_vec , sendcount , displs , MPI_INT ,
+                    local_vec , sendcount[rank] , MPI_INT , 0 , MPI_COMM_WORLD);
                     int local_max_diff = getSequentialOperations(local_vec, delta+1);
                     MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
                 }
             } else {
                 int* local_vec = new int[delta+1];
                 MPI_Status status;
-                MPI_Recv(local_vec, delta + 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+                MPI_Scatterv(global_vec , sendcount , displs , MPI_INT ,
+                    local_vec , sendcount[rank] , MPI_INT , 0 , MPI_COMM_WORLD);
                 int local_max_diff = getSequentialOperations(local_vec, delta+1);
                 MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
             }
