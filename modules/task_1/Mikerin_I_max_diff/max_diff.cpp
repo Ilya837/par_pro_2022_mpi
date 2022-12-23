f208abc6dbab702444590d3d0377118f77dc409e
Mikerin Ilya <mikerin-2002@mail.ru>
2022-12-23 20:31:02 +0300

    Микерин Илья. Задача 1. Вариант 8. Нахождение наиболее отличающихся по значению соседних элементов вектора

--- modules/task_1/Mikerin_I_max_diff/max_diff.cpp
+++ modules/task_1/Mikerin_I_max_diff/max_diff.cpp
@@ -34,10 +34,22 @@ int getParallelOperations(int* global_vec, int count_size_vector) {
     const int remainder = count_size_vector % size;
 
     if (rank == 0) {
-        for (int proc = 1; proc < size-1; proc++) {
-            MPI_Send(global_vec + proc * delta + remainder, delta+1, MPI_INT, proc, 0, MPI_COMM_WORLD);
+        if (remainder > 0) {
+            for (int proc =1; proc < remainder -1; proc++) {
+                MPI_Send(global_vec + proc * delta + proc, delta+2, MPI_INT, proc, 0, MPI_COMM_WORLD);
+            }
+            for (int proc = remainder - 1; proc < size-1; proc++) {
+                if (proc > 0) {
+                    MPI_Send(global_vec + proc * delta + remainder, delta+1, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                }
+            }
+            MPI_Send(global_vec + (size-1) * delta + remainder, delta, MPI_INT, (size-1), 0, MPI_COMM_WORLD);
+        } else {
+            for (int proc = 1; proc < size-1; proc++) {
+                MPI_Send(global_vec + proc * delta, delta+1, MPI_INT, proc, 0, MPI_COMM_WORLD);
+            }
+            MPI_Send(global_vec + (size-1) * delta, delta, MPI_INT, (size-1), 0, MPI_COMM_WORLD);
         }
-        MPI_Send(global_vec + (size-1) * delta + remainder, delta, MPI_INT, (size-1), 0, MPI_COMM_WORLD);
     }
 
     int global_max_diff = 0;
@@ -51,19 +63,36 @@ int getParallelOperations(int* global_vec, int count_size_vector) {
         MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
     } else {
         if (rank == 0) {
-            int* local_vec = new int[delta+1 + remainder];
-            for (int i = 0; i < delta+ 1 + remainder; i++) {
+            int* local_vec = new int[delta+1 + static_cast<int>(remainder > 0)];
+            for (int i = 0; i < delta+ 1 + static_cast<int>(remainder > 0); i++) {
                 local_vec[i] = global_vec[i];
             }
-            int local_max_diff = getSequentialOperations(local_vec, delta+1 + remainder);
+            int local_max_diff = getSequentialOperations(local_vec, delta+1 + static_cast<int>(remainder > 0));
             MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 
         } else {
-            int* local_vec = new int[delta+1];
-            MPI_Status status;
-            MPI_Recv(local_vec, delta + 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-            int local_max_diff = getSequentialOperations(local_vec, delta+1);
-            MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+            if (remainder > 0) {
+                if (rank < remainder - 1) {
+                    int* local_vec = new int[delta+2];
+                    MPI_Status status;
+                    MPI_Recv(local_vec, delta + 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+                    int local_max_diff = getSequentialOperations(local_vec, delta+2);
+                    MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+
+                } else {
+                    int* local_vec = new int[delta+1];
+                    MPI_Status status;
+                    MPI_Recv(local_vec, delta + 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+                    int local_max_diff = getSequentialOperations(local_vec, delta+1);
+                    MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+                }
+            } else {
+                int* local_vec = new int[delta+1];
+                MPI_Status status;
+                MPI_Recv(local_vec, delta + 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+                int local_max_diff = getSequentialOperations(local_vec, delta+1);
+                MPI_Reduce(&local_max_diff, &global_max_diff, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+            }
         }
     }
 
